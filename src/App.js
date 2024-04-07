35bef3501ed63bd3e74a4618e35e516da1b534fe
bobbyhadz <opmftw@gmail.com>
2024-04-07 13:44:12 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div
+        style={{
+          position: 'relative',
+          zIndex: '3',
+          backgroundColor: 'salmon',
+          padding: '2rem',
+        }}
+      >
+        bobbyhadz.com
+      </div>
+    </div>
+  );
+};
+
+export default App;
